50f05653da58d2b88ae13fc2acf84c1c4c8288de
Adrian Antanon Orozco <aantanon@brothapps.com>
2022-03-05 00:22:08 +0100

    feat: add moviedb url & key

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    moviedb_key: '2347257a3552ef655f75aceb97de0645',
+    moviedb_url: 'https://api.themoviedb.org/3/movie/550?api_key=',
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import { env } from '../next.config';
 import Billboard from '../src/components/Home/billboard/Billboard';
 
 export default function Home() {
